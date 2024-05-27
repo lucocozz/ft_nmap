27fe17b732c42067f8728b6efa7cec53130b5347
lucocozz <lucocozz@student.42.fr>
2024-05-27 18:33:16 +0200

    improve ips file parsing

--- sources/cli/handlers/arg_file_handler.c
+++ sources/cli/handlers/arg_file_handler.c
@@ -1,5 +1,34 @@
 #include "cli.h"
 
+
+static void	__insert_line(cli_builder_t *cli_builder, char *line)
+{
+	char **args = split(line, " ,;");
+	if (args == NULL)
+		return;
+
+	for (int i = 0; args[i] != NULL; ++i)
+		rbt_insert(cli_builder->targets, args[i]);
+	free_split(args);
+}
+
+
+static void	__read_file(cli_builder_t *cli_builder, FILE *file)
+{
+	size_t	len = 0;
+	char	*line = NULL;
+	ssize_t	read;
+	
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+		__insert_line(cli_builder, line);	
+	}
+	if (line != NULL)
+		free(line);
+}
+
 void	arg_file_handler(cli_builder_t *cli_builder, char *arg)
 {
 	FILE *file = fopen(arg, "r");
@@ -10,17 +39,6 @@ void	arg_file_handler(cli_builder_t *cli_builder, char *arg)
 		exit(EXIT_FAILURE);
 	}
 
-	size_t	len = 0;
-	char	*line = NULL;
-	ssize_t	read;
-	
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
-		rbt_insert(cli_builder->targets, line);
-	}
-	if (line != NULL)
-		free(line);
+	__read_file(cli_builder, file);
 	fclose(file);
 }
